04322af454db6fa31fc4305f9af3d04c5024e8ad
Trevor Waweru <trevor.k.x.waweru@gmail.com>
2025-06-30 10:11:33 +0300

    Correction: urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import run_migrations, home_view, CustomerListCreateAPIView, OrderListCreateAPIView, register_customer_view, order_form_view, logout_view, login_redirect_view
+from .views import run_migrations_view, home_view, CustomerListCreateAPIView, OrderListCreateAPIView, register_customer_view, order_form_view, logout_view, login_redirect_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -8,6 +8,6 @@ urlpatterns = [
     path('orders/', OrderListCreateAPIView.as_view(), name='order-list-create'),
     path('create-order/', order_form_view, name='order_form'),
     path('login-redirect/', login_redirect_view, name='login_redirect'),
-    path("run-migrations/", run_migrations),
+    path("run-migrations/", run_migrations_view),
     path('logout/', logout_view, name='logout'),
 ]
